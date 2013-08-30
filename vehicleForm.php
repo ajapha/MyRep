c4159e7feb607babf069cc87246fa69047b1f31e
ajapha <aaronjapha@gmail.com>
2013-08-30 14:57:28 -0400

    adding vehicle form program

---
+++ vehicleForm.php
@@ -0,0 +1,41 @@
+<?php
+include('linkedforms.php');
+
+$obj = new $showCarForm;
+
+class program {
+
+  public function showCarForm() {
+    echo $carForm;
+  }
+  public function showNameForm() {
+    echo $carform;
+  }
+
+  public $pages = $_REQUEST[];
+
+
+  public function __construct($page){
+    $form = new $pages; 
+  }
+
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
